9136c481f9758a15042c5bfb28220ded060c2633
Abhishek BHosale <abhisheksachinbhosale@gmail.com>
2023-08-17 21:47:16 +0530

    feat: Added opportunity-application and withdraw-application endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserOpportunityViewSet, OpportunityList
+from .views import UserOpportunityViewSet, OpportunityList, ApplyOpportunity, WithdrawApplication
 
 router = DefaultRouter()
 router.register('opportunities/me', UserOpportunityViewSet)
@@ -8,4 +8,6 @@ router.register('opportunities/all', OpportunityList)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('opportunities/<int:opp_id>/apply', ApplyOpportunity),
+    path('opportunities/<int:app_id>/withdraw', WithdrawApplication)
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,9 @@ from rest_framework import viewsets, generics, mixins
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
 from rest_framework import status
-from core.models import Opportunity, Opportunity_Type, Domain, Skill, User_Profile
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from core.models import Opportunity, Opportunity_Type, Domain, Skill, User_Profile, Application
 from .serializers import OpportunitySerializer, OpportunityTypeSerializer,SkillSerializer, DomainSerializer
 
 
@@ -20,7 +22,34 @@ class UserOpportunityViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+
 """ Endpoint to list, retrive all opportunities """
 class OpportunityList(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = Opportunity.objects.all()
     serializer_class = OpportunitySerializer
+
+
+""" Apply to opportunity """
+@api_view(['GET'])
+def ApplyOpportunity(request, opp_id):
+    user = request.user
+    opportunity = get_object_or_404(Opportunity, id=opp_id)
+    try:
+        application = Application.objects.get(applicant=user, opportunity=opportunity)
+        return Response({"detail": "You have already applied to this opportunity"}, status=status.HTTP_400_BAD_REQUEST)
+    except Application.DoesNotExist:
+        application = Application.objects.create(applicant=user, opportunity=opportunity)
+        return Response({"detail": "Application created successfully"}, status=status.HTTP_200_OK)
+
+
+""" Withdraw application - some changes required """
+@api_view(['GET'])
+def WithdrawApplication(request, app_id):
+    # first check if application exists with given id, then check if request.user and applicant are same and then do rest
+    user = request.user
+    try:
+        application = Application.objects.get(id=app_id, applicant=user)
+        application.delete()
+        return Response({"detail": "Application withdrawn successfully"}, status=status.HTTP_200_OK)
+    except Application.DoesNotExist:
+        return Response({"detail": "You have not applied to this opportunity"}, status=status.HTTP_400_BAD_REQUEST)
