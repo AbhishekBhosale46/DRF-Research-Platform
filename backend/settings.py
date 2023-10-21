671d88b3862a8db6cc173b55105f0b63575d47d5
Abhishek BHosale <abhisheksachinbhosale@gmail.com>
2023-10-21 09:00:23 +0530

    feat: customized django-admin using jet

--- backend/settings.py
+++ backend/settings.py
@@ -22,6 +22,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jet.dashboard',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend/urls.py
+++ backend/urls.py
@@ -6,6 +6,8 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
+    path('jet/', include('jet.urls', 'jet')),
+    path('jet/dashboard', include('jet.dashboard.urls', 'jet-dashboard')),
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-view'),
