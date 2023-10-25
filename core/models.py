c18296a3ea2c980fecda9ace37e112f8145e192b
Abhishek BHosale <abhisheksachinbhosale@gmail.com>
2023-10-25 09:52:07 +0530

    fix: modified feedback model

--- core/models.py
+++ core/models.py
@@ -123,10 +123,10 @@ class Application(models.Model):
 
 class Feedback(models.Model):
     email = models.EmailField()
-    subject = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     message = models.TextField()
 
     def __str__(self):
-        return f"{self.email} {self.subject}"
+        return f"{self.email} {self.name}"
     
 
